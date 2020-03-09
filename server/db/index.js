0802af773b940bd660330246c03a006ed90ec278
lokman <lokman-hekim@hotmail.fr>
2020-03-10 00:30:48 +0100

    ajout url addvehicule

--- server/db/index.js
+++ server/db/index.js
@@ -115,17 +115,21 @@ bmwdb.connect = (mail, mdp) => {
 bmwdb.addVehicule = (body, type) => {
     const { marque, modele, immatriculation, cylindree, prix, img1, img2, typeBoite, energie, typeVeh } = body;
     return new Promise((resolve, reject) => {
-        switch (type) {
-            case 'client':
-            pool.query(`call insert_veh_client(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);`,
-                [3, marque, modele, '2018-09-24', immatriculation, typeVeh, cylindree, energie, typeBoite, 'bon etat', '', 78500, img1, img2], (err, results) => {
-                if (err) {
-                    return [{ status: "error" }];
-                }
-                return [{ status: "success" }];
-            });
-            break;
-            default: break;
+        try {
+            switch (type) {
+                case 'client':
+                pool.query(`call insert_veh_client(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`,
+                    [3, marque, modele, '2018-09-24', immatriculation, typeVeh, cylindree, energie, typeBoite, 'bon etat', '', 78500, img1, img2], (err, results) => {
+                    if (err) {
+                        return reject(err);
+                    }
+                    return [{ status: "success" }];
+                });
+                break;
+                default: break;
+            }
+        } catch (error) {
+            return [{ error }];
         }
     });
 };
