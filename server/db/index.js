18dac64de56b0b8441409d17436eb716343762c7
lokman <lokman-hekim@hotmail.fr>
2020-03-10 00:21:15 +0100

    ajout url addvehicule

--- server/db/index.js
+++ server/db/index.js
@@ -117,8 +117,8 @@ bmwdb.addVehicule = (body, type) => {
     return new Promise((resolve, reject) => {
         switch (type) {
             case 'client':
-            pool.query(`call insert_veh_client(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);`,
-                [3, marque, modele, '2018-09-24', immatriculation, typeVeh, cylindree, energie, typeBoite, 'bon etat', 78500, img1, img2], (err, results) => {
+            pool.query(`call insert_veh_client(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);`,
+                [3, marque, modele, '2018-09-24', immatriculation, typeVeh, cylindree, energie, typeBoite, 'bon etat', '', 78500, img1, img2], (err, results) => {
                 if (err) {
                     return [{ status: "error" }];
                 }
